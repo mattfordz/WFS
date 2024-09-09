2ca5dd1531adddf93c8cfd729c144a216e681f8a
Matthew Fordan <matthewfordan111@gmail.com>
2024-09-09 18:48:48 +0800

    Add project files.

---
+++ WFS/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WFS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.IsLogin = true;
+            return View();
+        }
+    }
+}
---
+++ WFS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WFS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
